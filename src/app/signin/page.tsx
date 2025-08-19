44c2af82fe74d095bf0d21c776930c3e21a21fd5
sangwookpark <sangwookp9591@gmail.com>
2025-08-19 19:44:04 +0900

    feat : login page 폼 추가

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,3 +1,38 @@
+'use client';
+
+import { loginAction } from '@/features/login/action';
+import { redirect } from 'next/navigation';
+import { useActionState, useEffect, useRef } from 'react';
+
 export default function Page() {
-    return <div>로그인</div>;
+    const formRef = useRef<HTMLFormElement>(null);
+    const [state, formAction, isPending] = useActionState(loginAction, null);
+
+    useEffect(() => {
+        if (state?.status) {
+            redirect('/');
+        }
+    }, [state?.status]);
+
+    const onSumbit = () => {
+        formRef?.current?.requestSubmit();
+    };
+    return (
+        <div>
+            <form ref={formRef} action={formAction}>
+                <label htmlFor="loginId">ID : </label>
+                <input required type="text" name="loginId" />
+
+                <label htmlFor="loginPw">PW : </label>
+                <input required type="password" name="loginPw" />
+
+                {isPending ? <div>...</div> : <div onClick={onSumbit}>버튼</div>}
+                {!state?.status && (
+                    <div style={{ color: 'red' }}>
+                        {state?.error} {state?.el}
+                    </div>
+                )}
+            </form>
+        </div>
+    );
 }
