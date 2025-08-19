1feacde9677db7e55a5994d70508a2e3fa092c71
sangwookpark <sangwookp9591@gmail.com>
2025-08-19 22:47:48 +0900

    feat : images domain 추가

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 
 const withVanillaExtract = createVanillaExtractPlugin();
 const nextConfig: NextConfig = {
-    /* config options here */
+  /* config options here */
+  images: {
+    domains: ['i.pravatar.cc', 'http://localhost:3000'],
+  },
 };
 
 export default withVanillaExtract(nextConfig);
