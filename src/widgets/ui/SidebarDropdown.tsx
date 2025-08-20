0d24aa0cae79d43455605a7701490f07ebce59c1
sangwookpark <sangwookp9591@gmail.com>
2025-08-20 19:44:18 +0900

    feat : sidebar dropdown vanillaextract 로 분리

--- src/widgets/ui/SidebarDropdown.tsx
+++ src/widgets/ui/SidebarDropdown.tsx
@@ -23,89 +23,47 @@ export default function SidebarDropdown<T extends { id: number | string }>({
 }: SidebarDropdownProps<T>) {
   const currentItem = items?.find((item) => `${pathname}` === `/workspace/1/channel/${item?.id}`);
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px', color: 'white' }}>
-      <div
-        style={{
-          display: 'flex',
-          fontWeight: 'bold',
-          alignItems: 'center',
-          gap: '10px',
-          cursor: 'pointer',
-        }}
-      >
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
         <div className={styles.dropdownIcon({ active: isActive })} onClick={onClick}>
-          {'▶'}
+          ▶
         </div>
         <div>{title}</div>
       </div>
-      {isActive ? (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '10px',
-          }}
-        >
-          {items?.map((item) => {
-            const label = item[keyName] as unknown as string;
 
-            const isAtive = `${pathname}` === `/workspace/1/${type}/${item?.id}`;
-            const color = isAtive ? 'rgba(57,6,58, 1)' : '#ffffff';
-            const backgroundColor = isAtive ? 'rgba(249,237,255, 1)' : '#3f0e40';
+      {isActive ? (
+        <div className={styles.wrapper}>
+          {items.map((item) => {
+            const label = item[keyName] as unknown as string;
+            const isActiveItem = `${pathname}` === `/workspace/1/${type}/${item.id}`;
             return (
               <Link
-                key={item?.id}
-                href={`/workspace/1/${type}/${item?.id}`}
-                style={{
-                  display: 'flex',
-                  gap: '5px',
-                  borderRadius: '15px',
-                  textDecoration: 'none',
-                  color: color,
-                  padding: '5px 10px',
-                  backgroundColor: backgroundColor,
-                  transition: 'background-color 0.3s ease, color 0.3s ease',
-                }}
+                key={item.id}
+                href={`/workspace/1/${type}/${item.id}`}
+                className={isActiveItem ? styles.activeItem : styles.item}
               >
-                <ChannelIcon size={20} color={color} />
+                <ChannelIcon size={20} color={isActiveItem ? 'rgba(57,6,58, 1)' : '#ffffff'} />
                 {label}
               </Link>
             );
           })}
 
-          <div style={{ display: 'flex', alignItems: 'center', gap: '5px', padding: '3px 10px' }}>
-            <div
-              style={{
-                borderRadius: '2px',
-                padding: '2.5px',
-                backgroundColor: '#611f69',
-                color: 'white',
-              }}
-            >
+          <div className={styles.addRow}>
+            <div className={styles.plusBox}>
               <PlusIcon size={15} />
             </div>
-            채널 추가
+            {title} 추가
           </div>
         </div>
       ) : (
         <div>
           {currentItem ? (
-            <div
-              style={{
-                display: 'flex',
-                gap: '5px',
-                textDecoration: 'none',
-                color: 'rgba(57,6,58, 1)',
-                backgroundColor: 'rgba(249,237,255, 1)',
-                borderRadius: '15px',
-                padding: '3px 10px',
-              }}
-            >
+            <div className={styles.activeItem}>
               <ChannelIcon size={20} color="rgba(57,6,58, 1)" />
-              {currentItem?.[keyName] as unknown as string}
+              {currentItem[keyName] as unknown as string}
             </div>
           ) : (
-            <div></div>
+            <div />
           )}
         </div>
       )}
--- src/widgets/ui/sidebar-dropdown.css.ts
+++ src/widgets/ui/sidebar-dropdown.css.ts
@@ -1,5 +1,21 @@
-import * as styles from '@vanilla-extract/css';
+import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
+
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px',
+  color: 'white',
+});
+
+export const title = style({
+  display: 'flex',
+  fontWeight: 'bold',
+  alignItems: 'center',
+  gap: '10px',
+  cursor: 'pointer',
+});
+
 export const dropdownIcon = recipe({
   base: {
     transform: 'rotate(0deg)',
@@ -15,4 +31,40 @@ export const dropdownIcon = recipe({
       },
     },
   },
+});
+
+export const item = style({
+  display: 'flex',
+  gap: '5px',
+  color: '#ffffff',
+  borderRadius: '15px',
+  textDecoration: 'none',
+  textAlign: 'center',
+  padding: '5px 10px',
+  lineHeight: '24px',
+
+  transition: 'background-color 0.3s ease, color 0.3s ease',
+});
+
+export const activeItem = style([
+  item,
+  {
+    color: 'rgba(57,6,58, 1)',
+    backgroundColor: 'rgba(249,237,255, 1)',
+  },
+]);
+
+export const addRow = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '5px',
+  padding: '3px 10px',
+});
+
+export const plusBox = style({
+  borderRadius: '2px',
+  padding: '2.5px',
+  backgroundColor: '#611f69',
+  color: 'white',
+  cursor: 'pointer',
 });
