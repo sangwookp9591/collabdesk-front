b955b85571148d1c2dbcc1ee81ce1b364ee88696
sangwookpark <sangwookp9591@gmail.com>
2025-08-20 11:07:37 +0900

    feat : header 추가

---
+++ src/widgets/ui/Header.tsx
@@ -0,0 +1,5 @@
+import * as styles from './header.css';
+
+export default function Header() {
+  return <header className={styles.container}></header>;
+}
---
+++ src/widgets/ui/header.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  width: '100wh',
+  height: '30px',
+  justifyItems: 'center',
+  alignItems: 'center',
+  backgroundColor: '#350d36',
+});
