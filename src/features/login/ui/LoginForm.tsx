a341acf9d0872c730d94aa2448d0ae8267e46bc8
sangwookpark <sangwookp9591@gmail.com>
2025-08-21 22:40:47 +0900

    feat : loginform features로 분리

---
+++ src/features/login/ui/LoginForm.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import * as styles from './login-form.css';
+import Link from 'next/link';
+
+export default function LoginForm() {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const res = await signIn('credentials', {
+      redirect: false, // NextAuth 자동 리다이렉트 막기
+      email: formData.get('email'),
+      password: formData.get('password'),
+    });
+
+    if (res?.ok) {
+      router.push('/workspace/1/channel/1'); // 로그인 성공 시 이동
+    } else {
+      setError(res?.error || '로그인 실패');
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit} className={styles.formStyle}>
+      <label>Email</label>
+      <input required className={styles.inputStyle} type="text" name="email" />
+
+      <label>Password</label>
+      <input required className={styles.inputStyle} type="password" name="password" />
+
+      <button type="submit" className={styles.buttonStyle}>
+        로그인
+      </button>
+
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+
+      <Link href="/signup">회원가입</Link>
+    </form>
+  );
+}
