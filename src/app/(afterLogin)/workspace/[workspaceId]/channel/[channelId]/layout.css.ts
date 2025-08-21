1e6f45c351e4e304018a1063932fc8d43b09fe85
sangwookpark <sangwookp9591@gmail.com>
2025-08-21 13:04:13 +0900

    feat : layout 페이지 꽉차게 수정

--- src/app/(afterLogin)/workspace/[workspaceId]/channel/[channelId]/layout.css.ts
+++ src/app/(afterLogin)/workspace/[workspaceId]/channel/[channelId]/layout.css.ts
@@ -3,7 +3,7 @@ import { style } from '@vanilla-extract/css';
 export const chatPage = style({
   display: 'flex',
   flexDirection: 'column',
-  height: '100vh',
+  height: '100%',
 });
 
 export const topBar = style({
