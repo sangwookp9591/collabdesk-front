5ee7220064322d1fcb3ec8dba22b153e8c03904e
sangwookpark <sangwookp9591@gmail.com>
2025-08-21 16:35:09 +0900

    feat : header left, middle, right 영역 설정

--- src/widgets/ui/Header.tsx
+++ src/widgets/ui/Header.tsx
@@ -4,7 +4,23 @@ import * as styles from './header.css';
 export default function Header() {
   return (
     <header className={styles.container}>
-      <ThemeSelector />
+      {/* left  */}
+      <div className={styles.leftContainer}>
+        <ThemeSelector />
+      </div>
+
+      {/* middle */}
+      <div className={styles.middleContainer}>
+        <input
+          type="text"
+          style={{
+            width: '100%',
+          }}
+        ></input>
+      </div>
+
+      {/* right */}
+      <div className={styles.rightContainer}></div>
     </header>
   );
 }
--- src/widgets/ui/header.css.ts
+++ src/widgets/ui/header.css.ts
@@ -1,7 +1,29 @@
+import { themeTokens } from '@/shared/styles/theme.css';
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
+  display: 'flex',
   height: '40px',
   flexShrink: 0,
-  backgroundColor: 'red',
+  backgroundColor: themeTokens.colors.backgroundSecondary,
+});
+
+export const leftContainer = style({
+  display: 'flex',
+  flex: '1 0 auto',
+  justifyContent: 'flex-start',
+});
+
+export const middleContainer = style({
+  display: 'flex',
+  flex: '2 1 0',
+  minWidth: '280px',
+  maxWidth: '1000px',
+  height: '100%',
+  justifyContent: 'center',
+});
+
+export const rightContainer = style({
+  display: 'flex',
+  flex: '1 0 auto',
 });
