71e19583db7887d8f90114cb117565c6a79b700b
sangwookpark <sangwookp9591@gmail.com>
2025-08-21 18:06:29 +0900

    feat :SearchIcon추가

--- src/shared/ui/IconSvg.tsx
+++ src/shared/ui/IconSvg.tsx
@@ -261,3 +261,19 @@ export const HelpIcon: React.FC<IconProps> = ({ size = 48, color = 'white' }) =>
     ></path>
   </svg>
 );
+
+export const SearchIcon: React.FC<IconProps> = ({ size = 48, color = 'white' }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 20 20"
+    fill={color}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9 3a6 6 0 1 0 0 12A6 6 0 0 0 9 3M1.5 9a7.5 7.5 0 1 1 13.307 4.746l3.473 3.474a.75.75 0 1 1-1.06 1.06l-3.473-3.473A7.5 7.5 0 0 1 1.5 9"
+    ></path>
+  </svg>
+);
