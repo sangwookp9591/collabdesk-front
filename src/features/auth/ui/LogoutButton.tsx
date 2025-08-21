e9fbbdd3276b73de3e9612e9ff7eacad7e5e7f00
sangwookpark <sangwookp9591@gmail.com>
2025-08-21 23:15:31 +0900

    feat : signout 수정

--- src/features/auth/ui/LogoutButton.tsx
+++ src/features/auth/ui/LogoutButton.tsx
@@ -2,12 +2,10 @@
 
 import { Button } from '@/shared/ui';
 import { signOut } from 'next-auth/react';
-import { redirect } from 'next/navigation';
 
 export default function LogoutButton() {
-  const onClick = () => {
-    signOut();
-    redirect('/signin');
+  const onClick = async () => {
+    await signOut({ callbackUrl: '/signin' }); // 로그아웃 후 /signin 이동
   };
   return <Button title="logout" onClick={onClick}></Button>;
 }
