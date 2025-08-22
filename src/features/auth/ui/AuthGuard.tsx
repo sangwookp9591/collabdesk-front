e099fd6bfbbd2ee3ce9abcdd3d4864dff4587154
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 13:56:43 +0900

    refactor : guard임시 주석 처리

--- src/features/auth/ui/AuthGuard.tsx
+++ src/features/auth/ui/AuthGuard.tsx
@@ -13,22 +13,23 @@ export default function AuthGuard({ children, fallback }: AuthGuardProps) {
   const { data: session, status } = useSession();
   const router = useRouter();
 
-  useEffect(() => {
-    if (status === 'loading') {
-      return;
-    }
-    if (!session) {
-      router.push('/signin');
-    }
-  }, [session, status, router]);
+  console.log('AuthGuard : ', session);
+  //   useEffect(() => {
+  //     if (status === 'loading') {
+  //       return;
+  //     }
+  //     if (!session) {
+  //       router.push('/signin');
+  //     }
+  //   }, [session, status, router]);
 
-  if (status === 'loading') {
-    return <div>Loading...</div>;
-  }
+  //   if (status === 'loading') {
+  //     return <div>Loading...</div>;
+  //   }
 
-  if (!session) {
-    return fallback || <div>Redirecting...</div>;
-  }
+  //   if (!session) {
+  //     return fallback || <div>Redirecting...</div>;
+  //   }
 
   return <>{children}</>;
 }
