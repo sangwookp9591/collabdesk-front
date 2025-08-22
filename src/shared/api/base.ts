5fb27a30f11248fa73dca36f1ef529e2f0f5abfe
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 13:57:09 +0900

    feat : lib getssesion으로 수정

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getSession } from 'next-auth/react';
+import { getSession } from '../lib';
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || '/api',
