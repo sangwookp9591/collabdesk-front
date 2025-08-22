0a0df9c8658b7273e88221ea7d069296dd8a65ad
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 13:05:41 +0900

    feat : mock setup list에 userHandler, workspaceHandler추가

--- src/shared/lib/msw/browser.ts
+++ src/shared/lib/msw/browser.ts
@@ -2,9 +2,11 @@
 import { setupWorker } from 'msw/browser';
 import { authHandlers } from './handlers/auth';
 import { oauthHandlers } from './handlers/oauth';
+import { userHandlers } from './handlers/user';
+import { workspaceHandlers } from './handlers/workspace';
 
 export let worker: ReturnType<typeof setupWorker> | undefined;
 
 if (typeof window !== 'undefined') {
-  worker = setupWorker(...authHandlers, ...oauthHandlers);
+  worker = setupWorker(...authHandlers, ...oauthHandlers, ...userHandlers, ...workspaceHandlers);
 }
