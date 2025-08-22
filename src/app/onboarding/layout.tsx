5a28888fef61af67198efbe14ec0b2a9c4241582
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 13:20:24 +0900

    feat : onboarding page에 logout button추가

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -1,5 +1,15 @@
+import { LogoutButton } from '@/features/auth';
 import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <header style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <div>
+          <LogoutButton />
+        </div>
+      </header>
+      {children}
+    </>
+  );
 }
