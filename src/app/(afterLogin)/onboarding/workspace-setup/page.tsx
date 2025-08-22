b7eaf736766d413303c9b8794ee53d3a3856a85b
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 13:59:45 +0900

    refactor  :mock api사용을 위해  sc를 cs로 변경

--- src/app/(afterLogin)/onboarding/workspace-setup/page.tsx
+++ src/app/(afterLogin)/onboarding/workspace-setup/page.tsx
@@ -1,8 +1,24 @@
-import { userApi } from '@/entities/user/api/userApi';
-import { getSession } from 'next-auth/react';
+'use client';
 
-export default async function Page() {
-  const session = await getSession();
+import { userApi } from '@/entities/user/api/userApi';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { workspace } from '@/widgets/sidebar/ui/sidemenu.css';
+
+export default function Page() {
+  const { data: session } = useSession();
+  const [workspaces, setWorkspaces] = useState([]);
+
+  useEffect(() => {
+    if (session?.user) {
+      const fn = async () => {
+        const workspaces = await userApi.getUserWorkspaces(session.user.id);
+        console.log('workspaces : ', workspaces);
+        setWorkspaces(workspaces?.workspaces);
+      };
+      fn();
+    }
+  }, [session?.user]);
 
   if (!session?.user) {
     return (
@@ -11,7 +27,7 @@ export default async function Page() {
       </div>
     );
   }
-  const workspaces = await userApi.getUserWorkspaces(session.user.id);
+
   return (
     <div>
       <div>보유한 workspace있음.</div>
