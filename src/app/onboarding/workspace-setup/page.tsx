f1441c23e3f80e2d5f512c986c5753c1ac9e3cc6
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 13:06:40 +0900

    feat : 온보딩 페이지 workspace 추가

--- src/app/onboarding/workspace-setup/page.tsx
+++ src/app/onboarding/workspace-setup/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div></div>;
+import { userApi } from '@/entities/user/api/userApi';
+import { getSession } from 'next-auth/react';
+
+export default async function Page() {
+  const session = await getSession();
+
+  if (!session?.user) {
+    return (
+      <div>
+        <div>보유한 workspace 없음</div>
+      </div>
+    );
+  }
+  const workspaces = await userApi.getUserWorkspaces(session.user.id);
+  return (
+    <div>
+      <div>보유한 workspace있음.</div>
+      <div>
+        {workspaces?.map((item: any) => (
+          <div key={item?.id}>{item?.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
