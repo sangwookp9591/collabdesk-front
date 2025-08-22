21a4bfa6964c8e723275ab224372c7c50e7ad723
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 17:15:07 +0900

    feat : infoCard에 className 추가

--- src/entities/workspace/ui/InfoCard.tsx
+++ src/entities/workspace/ui/InfoCard.tsx
@@ -2,16 +2,18 @@ import { Avatar } from '@/entities/workspace';
 import * as styles from './info-card.css';
 import Image from 'next/image';
 import { WorkspaceCard } from '../model/type';
+import { clsx } from 'clsx';
 
 type InfoCardProps = {
   workspace: WorkspaceCard;
   onClick?: (id: string) => void;
+  className?: string;
 };
 
-export function InfoCard({ workspace, onClick }: InfoCardProps) {
+export function InfoCard({ workspace, onClick, className }: InfoCardProps) {
   return (
     <div
-      className={styles.workspaceContainer}
+      className={clsx(styles.workspaceContainer, className)}
       onClick={onClick ? () => onClick(workspace?.id) : () => {}}
     >
       <Avatar url={workspace?.image} name={workspace?.name} size={50} />
