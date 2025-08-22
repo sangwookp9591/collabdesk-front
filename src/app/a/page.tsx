24d810c276e71304ebde9870fc332337bebde2ce
sangwookpark <sangwookp9591@gmail.com>
2025-08-22 12:12:05 +0900

    refactor : 미사용 폴더 삭제

--- src/app/a/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Page() {
-    return (
-        <div>
-            <Link href={'/b'}>B로이동</Link>
-        </div>
-    );
-}
--- src/app/b/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <div>B페이지</div>;
-}
