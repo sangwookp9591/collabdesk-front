3f231c70d732eeb4210db4c334db6b7be578af19
sangwookpark <sangwookp9591@gmail.com>
2025-08-28 16:53:06 +0900

    refactor : message item으로 분리

---
+++ src/entities/message/ui/MessageItem.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { Avatar } from '@/entities/user';
+import * as styles from './message-item.css';
+import { format } from 'date-fns';
+import { Message } from '@/shared/types/message';
+
+type MessageItemProps = {
+  message: Message;
+  isSameUserWithinMinute: boolean;
+};
+
+export default function MessageItem({ message, isSameUserWithinMinute }: MessageItemProps) {
+  const currentDate = new Date(message.createdAt);
+
+  return (
+    <div key={message.id} className={styles.messageItem}>
+      {!isSameUserWithinMinute ? (
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <Avatar
+            isActive={message.user.status === 'ONLINE'}
+            profileImageUrl={message.user.profileImageUrl ?? '/images/default_profile.png'}
+            name={message?.user?.name || ''}
+            size={48}
+          />
+          <div style={{ display: 'flex', flexDirection: 'column', gap: '5px', width: '100%' }}>
+            <div style={{ display: 'flex', gap: '10px' }}>
+              <span style={{ fontSize: '1.1rem', fontWeight: 'bold' }}>{msg.user.name}</span>
+              <span style={{ fontSize: '0.8rem', color: '#ADB5BD' }}>
+                {format(currentDate, 'a h:mm')}
+              </span>
+            </div>
+            {message.content}
+          </div>
+        </div>
+      ) : (
+        <div style={{ marginLeft: '58px', marginTop: '2px' }}>{message.content}</div>
+      )}
+    </div>
+  );
+}
---
+++ src/entities/message/ui/message-item.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const messageItem = style({
+  padding: '8px 12px',
+  borderRadius: '8px',
+  maxWidth: '60%',
+});
