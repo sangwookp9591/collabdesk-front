8f9acc416c970329dc4a86d1bfa04c4962b98b58
sangwookpark <sangwookp9591@gmail.com>
2025-08-29 01:46:34 +0900

    refactor : message socket message들어올때 handler추가

--- src/app/(afterLogin)/workspace/[wsSlug]/channel/[chSlug]/page.tsx
+++ src/app/(afterLogin)/workspace/[wsSlug]/channel/[chSlug]/page.tsx
@@ -1,19 +1,33 @@
 'use client';
 
-import { useChannelMessages } from '@/entities/message/api/queries/message.queries';
+import { useChannelMessages } from '@/entities/message/model/message.queries';
+import { useSocketStore } from '@/entities/message/model/socket.store';
 import MessageSend from '@/features/message-send/ui/MessageSend';
 import MessageList from '@/widgets/message/ui/MessageList';
 import { useParams } from 'next/navigation';
+import { useWorkspaceStore } from '../../../../../../shared/stores/workspace-store';
+import { useCallback } from 'react';
 
 export default function Page() {
   const params = useParams();
 
   const chSlug = params?.chSlug as string;
-  const { data, isLoading, error } = useChannelMessages(chSlug);
+  const { currentChannel } = useWorkspaceStore();
+  const { data, isLoading, error } = useChannelMessages(chSlug, currentChannel?.id);
+
+  const { sendMessage } = useSocketStore();
+  // };
+
+  const handleSendMessaage = useCallback(
+    (content: string) => {
+      if (currentChannel?.id) sendMessage({ channelId: currentChannel?.id, content: content });
+    },
+    [currentChannel?.id],
+  );
   return (
     <>
       <MessageList messages={data?.messages || []} isLoading={isLoading} />
-      <MessageSend onSend={() => {}} />
+      <MessageSend onSend={handleSendMessaage} />
     </>
   );
 }
--- src/entities/message/model/message.queries.ts
+++ src/entities/message/model/message.queries.ts
@@ -1,14 +1,47 @@
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery, useQueryClient } from '@tanstack/react-query';
 import { messageApi } from '../api/message.api';
 import { messageKeys } from './message-keys';
+import { useSocketStore } from './socket.store';
+import { useEffect } from 'react';
 
-export const useChannelMessages = (slug: string, page: number = 1, take?: number) => {
-  return useQuery({
+export const useChannelMessages = (
+  slug: string,
+  channelId?: string,
+  page: number = 1,
+  take?: number,
+) => {
+  const socket = useSocketStore((state) => state.socket);
+  const queryClient = useQueryClient();
+  const query = useQuery({
     queryKey: messageKeys.channelMessages(slug, page),
     queryFn: () => messageApi.getMessagesByChannel(slug, page, take),
     staleTime: 1000 * 60, // 1분
     enabled: !!slug,
   });
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const handler = (message: any) => {
+      // React Query cache 업데이트
+      if (channelId && message.channelId) {
+        queryClient.setQueryData(messageKeys.channelMessages(slug, page), (old: any) => {
+          console.log('old: ', old);
+          // old가 배열인지 확인
+
+          return { ...old, messages: [...old.messages, message], total: Number(old.total) + 1 };
+        });
+      }
+    };
+
+    socket.on('newMessage', handler);
+
+    return () => {
+      socket.off('newMessage', handler);
+    };
+  }, [socket, channelId, slug, page, queryClient]);
+
+  return query;
 };
 
 export const useInfiniteChannelMessages = (slug: string) => {
