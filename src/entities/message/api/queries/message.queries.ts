fe60b08bf6ed78cf64f86d0afff9662987eff0b1
sangwookpark <sangwookp9591@gmail.com>
2025-08-28 19:37:31 +0900

    feat : message querys 추가

---
+++ src/entities/message/api/queries/message.queries.ts
@@ -0,0 +1,24 @@
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { messageApi } from '../message.api';
+import { messageKeys } from './message-keys';
+
+export const useChannelMessages = (slug: string, page: number = 1) => {
+  return useQuery({
+    queryKey: messageKeys.channelMessages(slug, page),
+    queryFn: () => messageApi.getMessagesByChannel(slug, page),
+    staleTime: 1000 * 60, // 1분
+    enabled: !!slug,
+  });
+};
+
+export const useInfiniteChannelMessages = (slug: string) => {
+  return useInfiniteQuery({
+    queryKey: messageKeys.channel(slug),
+    queryFn: ({ pageParam = 1 }) => messageApi.getMessagesByChannel(slug, pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.hasMore ? allPages.length + 1 : undefined;
+    },
+    enabled: !!slug,
+  });
+};
