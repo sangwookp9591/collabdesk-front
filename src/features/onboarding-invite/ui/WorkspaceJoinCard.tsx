010ccdf49df2ef96e01703f2d19972bbb95fb2f4
sangwookpark <sangwookp9591@gmail.com>
2025-08-31 22:33:08 +0900

    feat: joinCard 추가

--- src/features/onboarding-invite/ui/WorkspaceJoinCard.tsx
+++ src/features/onboarding-invite/ui/WorkspaceJoinCard.tsx
@@ -4,13 +4,28 @@ import { InfoCard } from '@/entities/workspace';
 import * as styles from './workspace-join-card.css';
 import { useRouter } from 'next/navigation';
 import { Workspace } from '@/shared/types/workspace';
+import { apiFetch } from '@/shared/api';
+import { useSession } from 'next-auth/react';
 
-export function WorkspaceJoinCard({ workspace }: { workspace: Workspace }) {
+export function WorkspaceJoinCard({ workspace, code }: { workspace: Workspace; code: string }) {
+  const { data: session } = useSession();
   const router = useRouter();
 
-  const joinWorkspace = () => {
+  const joinWorkspace = async () => {
     // 내 워크스페이스에 추가 후 , 최근 워크스페이스 아이디 저장
 
+    const res = await apiFetch(`/workspace/join`, {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({ code }),
+      headers: {
+        Authorization: `Bearer ${session?.user?.accessToken}`,
+      },
+    });
+
+    if (!res?.ok) {
+      return;
+    }
     router.replace(`/workspace/${workspace.id}`);
   };
   return (
