e68ec929bb9575702243d5d1272bb7579f7fa119
sangwookpark <sangwookp9591@gmail.com>
2025-09-01 00:14:22 +0900

    feat : 기본 워크스페이스 부분 추가

--- src/app/(afterLogin)/workspace/[wsSlug]/page.tsx
+++ src/app/(afterLogin)/workspace/[wsSlug]/page.tsx
@@ -1,3 +1,15 @@
 export default function Page() {
-  return <div>워크스페이스</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        alignContent: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      워크스페이스
+    </div>
+  );
 }
