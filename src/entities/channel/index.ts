f56a9dd3b0f86c38e288e114f2efda1e1c9a2983
sangwookpark <sangwookp9591@gmail.com>
2025-09-02 14:49:57 +0900

    feat :index에 querys, keys 추가

--- src/entities/channel/index.ts
+++ src/entities/channel/index.ts
@@ -1,2 +1,4 @@
-export { channelApi } from './api/channelApi';
+export { channelApi } from './api/channel.api';
+export * as queries from './model/channel.queries';
+export { CHANNEL_QUERY_KEYS } from './model/channel-keys';
 export { default as ChannelTitle } from './ui/ChannelTitle';
