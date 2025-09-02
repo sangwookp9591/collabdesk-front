ac3c86951e66b7213aa68545ff771c8d8c3053be
sangwookpark <sangwookp9591@gmail.com>
2025-09-02 15:48:22 +0900

    refactor : workspace entiites model, api삭제

--- src/entities/workspace/api/workspaceApi.ts
+++
@@ -1,9 +0,0 @@
-import { apiClient } from '@/shared/api/base';
-import { WorkspaceSidebar } from '../model/type';
-
-export const workspaceApi = {
-  fetchWorkspaceSidebar: async (workspaceId: string): Promise<WorkspaceSidebar> => {
-    const response = await apiClient.get(`/workspace/sidebar/${workspaceId}`);
-    return response.data;
-  },
-};
--- src/entities/workspace/index.ts
+++ src/entities/workspace/index.ts
@@ -1,5 +1,3 @@
-export { workspaceApi } from './api/workspaceApi';
-export { type WorkspaceSidebar } from './model/type';
 export { Avatar } from './ui/Avatar';
 export { InfoCard } from './ui/InfoCard';
 export { InfoCardSkeleton } from './ui/InfoCardSkeleton';
