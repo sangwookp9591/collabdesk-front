266c3c5706eef290ba5c282d88816a062baa61b7
sangwookpark <sangwookp9591@gmail.com>
2025-09-03 00:25:07 +0900

    feat : realtime channelDelete 추가

--- src/shared/hooks/useRealtimeEvents.ts
+++ src/shared/hooks/useRealtimeEvents.ts
@@ -11,7 +11,7 @@ import { CHANNEL_QUERY_KEYS } from '@/entities/channel';
 export const useRealtimeSubEvents = () => {
   const queryClient = useQueryClient();
   const { socket } = useSocketStore();
-  const { addChannel, currentWorkspace } = useWorkspaceStore();
+  const { addChannel, currentWorkspace, deleteChannel } = useWorkspaceStore();
 
   useEffect(() => {
     console.log('socket realtime : ', currentWorkspace);
@@ -26,13 +26,34 @@ export const useRealtimeSubEvents = () => {
       addChannel(message);
     };
 
+    const handleChannelDeletedEvent = (message: {
+      workspaceId: string;
+      channelId: string;
+      userId: string;
+      message: string;
+    }) => {
+      console.log('channel 체널삭제구독 수신 !!! :', message);
+      queryClient.invalidateQueries({
+        queryKey: [CHANNEL_QUERY_KEYS.allByWsId(currentWorkspace?.id)],
+      });
+
+      deleteChannel(message?.channelId);
+      console.log('message: ', message.message);
+    };
+
+    const handleChannelUpdateEvent = () => {};
+
     // ADD Event Listener
     socket.on(EVENT_KEYS.SUB_CHANNEL_CREATED, handleChannelCreatedEvent);
+    socket.on(EVENT_KEYS.SUB_CHANNEL_DELETED, handleChannelDeletedEvent);
+    socket.on(EVENT_KEYS.SUB_CHANNEL_UPDATED, handleChannelUpdateEvent);
 
     return () => {
       socket.off(EVENT_KEYS.SUB_CHANNEL_CREATED);
+      socket.off(EVENT_KEYS.SUB_CHANNEL_DELETED);
+      socket.off(EVENT_KEYS.SUB_CHANNEL_UPDATED);
     };
-  }, [socket, queryClient, currentWorkspace, addChannel]);
+  }, [socket, queryClient, currentWorkspace, addChannel, deleteChannel]);
 
   return {};
 };
