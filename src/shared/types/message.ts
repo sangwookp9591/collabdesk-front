4c3d6acc2f6cc8dcf6423500fd53126cd81328e6
sangwookpark <sangwookp9591@gmail.com>
2025-09-02 19:38:33 +0900

    feat : message type추가로인한 메세지 파트 시스템 봇 구분 추가

--- src/shared/types/message.ts
+++ src/shared/types/message.ts
@@ -1,6 +1,6 @@
 import { Channel } from './channel';
 import { User } from './user';
-
+type messageType = 'USER' | 'SYSTEM' | 'BOT';
 export interface Message {
   id: string;
   content: string;
@@ -12,6 +12,8 @@ export interface Message {
   createdAt: Date;
   updatedAt: Date;
   channel?: Channel;
+  messageType: messageType;
+  systemData?: any;
   user: Omit<
     User,
     | 'password'
