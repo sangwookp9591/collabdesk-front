0dea3931871e6f4146c932badeda7f16dfa1e9eb
sangwookpark <sangwookp9591@gmail.com>
2025-09-03 20:04:10 +0900

    refactor : worskpace invite기능 api수정

--- src/entities/invite/api/invite.api.ts
+++ src/entities/invite/api/invite.api.ts
@@ -1,12 +1,27 @@
 import { ApiBase } from '@/shared/api';
 import { ChannelRole } from '@/shared/types/channel';
+import { WorkspaceRole } from '@/shared/types/workspace';
 
 class InviteApi extends ApiBase {
+  // 코드로 워크스페이스 조회
   async getWorkspaceByInvite(type: string, code: string) {
     return await this.fetchWithAuth(`/${type}?code=${code}`, {
       method: 'GET',
     });
   }
+
+  //워크스페이스 초대
+  async workspaceInvite(dto: { email: string; workspaceId: string; workspaceRole: WorkspaceRole }) {
+    return await this.fetchWithAuth(`/workspace`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dto),
+    });
+  }
+
+  // 채널 초대
   async channelInvite(dto: {
     email: string;
     workspaceId: string;
@@ -15,6 +30,9 @@ class InviteApi extends ApiBase {
   }) {
     return await this.fetchWithAuth(`/channel`, {
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify(dto),
     });
   }
--- src/features/invite/model/workspace-invite-action.ts
+++ src/features/invite/model/workspace-invite-action.ts
@@ -1,17 +1,11 @@
 'use server';
 
-import { apiFetch } from '@/shared/api';
-import { getSession, validateDto } from '@/shared/lib';
+import { validateDto } from '@/shared/lib';
 import { InviteDto } from './invite.dto';
 import { WorkspaceRole } from '@/shared/types/workspace';
+import { inviteApi } from '@/entities/invite';
 
 export async function workspaceInviteAction(_: any, formData: FormData) {
-  const session = await getSession();
-
-  if (!session?.user) {
-    throw new Error('Authentication required');
-  }
-  console.log('formData: ', formData);
   const email = formData.get('email')?.toString();
   const workspaceId = formData.get('workspaceId')?.toString();
   const workspaceRole = formData.get('workspaceRole') as WorkspaceRole;
@@ -22,34 +16,25 @@ export async function workspaceInviteAction(_: any, formData: FormData) {
     workspaceRole,
   });
 
-  console.log('validation : ', validation);
   if (!validation.status) {
     return validation;
   }
 
-  console.log('요청');
-  const res = await apiFetch(`/workspace/invite`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${session.user?.accessToken}`,
-    },
-    body: JSON.stringify({ email, workspaceId, workspaceRole }),
-  });
-
-  console.log('res : ', res);
-  if (!res?.ok) {
+  try {
+    const result = await inviteApi.workspaceInvite({
+      email: email!,
+      workspaceId: workspaceId!,
+      workspaceRole,
+    });
+    return {
+      status: true,
+      error: '',
+      data: result,
+    };
+  } catch (error: any) {
     return {
       status: false,
-      error: `${res.statusText}`,
+      error: error?.message,
     };
   }
-
-  const result = await res.json();
-  return {
-    status: true,
-    error: '',
-    data: result,
-  };
 }
