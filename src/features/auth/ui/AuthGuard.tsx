8ae24da6a2bc9db127f0ed814d87bd29fa6b82e4
sangwookpark <sangwookp9591@gmail.com>
2025-09-03 18:09:37 +0900

    feat : auth guard 세션로딩전이면 아에 클라이언트 안보여지도록 수정

--- src/features/auth/ui/AuthGuard.tsx
+++ src/features/auth/ui/AuthGuard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { redirect, useParams, usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
 interface AuthGuardProps {
@@ -11,11 +11,14 @@ interface AuthGuardProps {
 
 export default function AuthGuard({ children }: AuthGuardProps) {
   const { data: session, status } = useSession();
+
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
+    console.log('session?.user : ', session?.user, status);
+
     if (status === 'loading') {
       return;
     }
@@ -29,12 +32,19 @@ export default function AuthGuard({ children }: AuthGuardProps) {
           'invite-path',
           `${pathname}?type=${searchParams.get('type')}&code=${searchParams.get('code')}`,
         );
-        redirect('/signin');
       }
-      console.log('pathname : ', pathname);
-      // router.push('/signin');
+      console.log('리다이렉트');
+      router.replace('/signin');
     }
   }, [session, status, router, pathname, searchParams]);
+
+  if (status === 'loading') {
+    return null; // 로딩 화면 표시 가능
+  }
+
+  if (status === 'unauthenticated') {
+    return null; // 페이지 렌더링 차단 후 useEffect에서 이동 처리
+  }
 
   return <>{children}</>;
 }
