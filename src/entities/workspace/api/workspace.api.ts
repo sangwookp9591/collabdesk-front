c7e72fd712f56ce13d626ccba28d87a1daf94e41
sangwookpark <sangwookp9591@gmail.com>
2025-09-03 16:51:30 +0900

    feat : workspace api 추가

--- src/entities/workspace/api/workspace.api.ts
+++ src/entities/workspace/api/workspace.api.ts
@@ -3,7 +3,29 @@ import { ApiBase } from '@/shared/api';
 export class WorkspaceApi extends ApiBase {
   async create() {}
 
-  async bySlug() {}
+  async bySlug(wsSlug: string) {
+    return await this.fetchWithAuth(`/workspaces/${wsSlug}`, {
+      method: 'GET',
+    });
+  }
+
+  async myMemberShip(wsSlug: string) {
+    return await this.fetchWithAuth(`/workspaces/${wsSlug}/members/me`, {
+      method: 'GET',
+    });
+  }
+
+  async userMemberShip(wsSlug: string, userId: string) {
+    return await this.fetchWithAuth(`/workspaces/${wsSlug}/members/${userId}`, {
+      method: 'GET',
+    });
+  }
+
+  async getWorkspaceStats(wsSlug: string) {
+    return await this.fetchWithAuth(`/workspaces/${wsSlug}/stats`, {
+      method: 'GET',
+    });
+  }
 }
 
 export const workspaceApi = new WorkspaceApi();
