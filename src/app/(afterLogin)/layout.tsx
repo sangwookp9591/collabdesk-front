f3a4b177f9df57a2d8c3db60aabe95a0367ebeb4
sangwookpark <sangwookp9591@gmail.com>
2025-09-04 19:55:38 +0900

    refactor : authguard import path수정

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,5 +1,5 @@
+import { AuthGuard } from '@/features/auth';
 import { ReactNode } from 'react';
-import AuthGuard from '../../features/auth/ui/AuthGuard';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <AuthGuard fallback={<div>loading</div>}>{children}</AuthGuard>;
