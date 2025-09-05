986e36d5e7ba45d1df97a172a20e246f653066fe
sangwookpark <sangwookp9591@gmail.com>
2025-09-06 01:59:36 +0900

    feat : 채널 초기화에 joinRoom추가

--- src/features/channel-initializer/ChannelInitializer.tsx
+++ src/features/channel-initializer/ChannelInitializer.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useSocketStore } from '@/entities/message';
 import { useWorkspaceStore } from '@/shared/stores/workspace-store';
 import { useParams } from 'next/navigation';
 import { ReactNode, useEffect } from 'react';
@@ -8,15 +9,18 @@ export default function ChannelInitializer({ children }: { children: ReactNode }
   const params = useParams();
   const chSlug = params?.chSlug as string;
 
-  const { channels, currentChannel, setCurrentChannel } = useWorkspaceStore();
+  const { channels, currentChannel, setCurrentChannel, setCurrentDm } = useWorkspaceStore();
+  const { joinRoom } = useSocketStore();
   const targetChannel = channels.find((c) => c.slug === chSlug);
 
   useEffect(() => {
     if (targetChannel && (!currentChannel || currentChannel.id !== targetChannel.id)) {
       console.log('채널 초기화:', targetChannel.name);
       setCurrentChannel(targetChannel);
-      // joinChannel(targetChannel.id);
+      setCurrentDm(null);
+      // 채널 소켓 연결
+      joinRoom(targetChannel.id, 'dm');
     }
-  }, [targetChannel, currentChannel, setCurrentChannel]);
+  }, [targetChannel, currentChannel, setCurrentChannel, setCurrentDm, joinRoom]);
   return <>{children}</>;
 }
