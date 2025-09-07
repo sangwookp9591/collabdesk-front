0ec1c98e67f659b51318afef0714de9102a53552
sangwookpark <sangwookp9591@gmail.com>
2025-09-07 21:03:24 +0900

    refactor  : layout auth guard삭제

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,6 +1,5 @@
-import { AuthGuard } from '@/features/auth';
 import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <AuthGuard fallback={<div>loading</div>}>{children}</AuthGuard>;
+  return <>{children}</>;
 }
