9b34197453f72044079055127b36bae919a59631
sangwookpark <sangwookp9591@gmail.com>
2025-09-07 21:04:56 +0900

    feat : home page loading content 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { redirect } from 'next/navigation';
 import { getSession } from '@/shared/lib';
 import { userApi } from '@/entities/user';
+import { Suspense } from 'react';
+import { LoadingConent } from '@/shared/ui';
+import { PageRoute } from '@/features/page-route';
 
 export default async function HomePage() {
   const session = await getSession();
@@ -11,10 +14,25 @@ export default async function HomePage() {
   }
 
   const result = await userApi.lastworkspace();
-  if (result.data?.lastActiveWorkspaceId) {
-    redirect(`/workspace/${result?.data?.workspaceSlug}`);
-  } else {
-    // 워크스페이스가 없으면 온보딩으로
-    redirect('/onboarding/workspace-setup');
-  }
+
+  return (
+    <Suspense
+      fallback={
+        <LoadingConent
+          mainTitle="이용자 정보를 불러오는 중..."
+          subTitle="잠시만 기다려 주세요"
+          cardTitle="사용자 인증 확인"
+          cardSubtitle="워크스페이스 정보를 가져오고 있습니다<"
+        />
+      }
+    >
+      <PageRoute
+        path={
+          result.data?.lastActiveWorkspaceId
+            ? `/workspace/${result?.data?.workspaceSlug}`
+            : '/onboarding/workspace-setup'
+        }
+      />
+    </Suspense>
+  );
 }
