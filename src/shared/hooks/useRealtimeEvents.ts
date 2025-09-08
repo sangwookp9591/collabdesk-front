4b886e8b21232139d56fb9b62b0a0dfec8ba61b3
sangwookpark <sangwookp9591@gmail.com>
2025-09-08 19:56:21 +0900

    feat:  user status  realtime events 추가

--- src/shared/hooks/useRealtimeEvents.ts
+++ src/shared/hooks/useRealtimeEvents.ts
@@ -8,15 +8,22 @@ import { Channel } from '../types/channel';
 import { useQueryClient } from '@tanstack/react-query';
 import { CHANNEL_QUERY_KEYS } from '@/entities/channel';
 import { DMConversation, DM_QUERY_KEYS } from '@/entities/dm';
+import { UserStatus } from '../types/user';
 
 export const useRealtimeSubEvents = () => {
   const queryClient = useQueryClient();
   const { socket } = useSocketStore();
-  const { addChannel, currentWorkspace, deleteChannel, addDm } = useWorkspaceStore();
+  const { addChannel, currentWorkspace, deleteChannel, addDm, setUserStatuses, updateUserStatus } =
+    useWorkspaceStore();
 
   useEffect(() => {
     console.log('socket realtime : ', currentWorkspace);
     if (!(socket && currentWorkspace)) return;
+
+    const handleJoinedWorkspace = (message: { workspaceId: string; userStatuses: any }) => {
+      console.log('handleJoinedWorkspace message : ', message);
+      setUserStatuses(message.userStatuses);
+    };
 
     const handleChannelCreatedEvent = (message: Channel) => {
       console.log('channel 체널생성 구독 수신 !!! :', message);
@@ -52,19 +59,34 @@ export const useRealtimeSubEvents = () => {
       addDm(conversation);
     };
 
+    const handleUpdateMemberStatus = (message: {
+      userId: string;
+      status: UserStatus;
+      lastActiveAt: Date;
+    }) => {
+      console.log('handleUpdateMemberStatus ', message);
+      if (message?.userId) {
+        updateUserStatus(message);
+      }
+    };
+
     // ADD Event Listener
+    socket.on(EVENT_KEYS.SUB_JOIN_WORKSPACE, handleJoinedWorkspace);
     socket.on(EVENT_KEYS.SUB_CHANNEL_CREATED, handleChannelCreatedEvent);
     socket.on(EVENT_KEYS.SUB_CHANNEL_DELETED, handleChannelDeletedEvent);
     socket.on(EVENT_KEYS.SUB_CHANNEL_UPDATED, handleChannelUpdateEvent);
     socket.on(EVENT_KEYS.SUB_DM_ROOM_CREATED, handleDmRoomCreadtedEvent);
+    socket.on(EVENT_KEYS.SUB_UPDATE_STATUS, handleUpdateMemberStatus);
 
     return () => {
+      socket.off(EVENT_KEYS.SUB_JOIN_WORKSPACE);
       socket.off(EVENT_KEYS.SUB_CHANNEL_CREATED);
       socket.off(EVENT_KEYS.SUB_CHANNEL_DELETED);
       socket.off(EVENT_KEYS.SUB_CHANNEL_UPDATED);
       socket.off(EVENT_KEYS.SUB_DM_ROOM_CREATED);
+      socket.off(EVENT_KEYS.SUB_DM_ROOM_CREATED);
     };
-  }, [socket, queryClient, currentWorkspace, addChannel, deleteChannel, addDm]);
+  }, [socket, queryClient, currentWorkspace, addChannel, deleteChannel, addDm, updateUserStatus]);
 
   return {};
 };
