7f7de0e00bd121660638609c3544a5cd3f2a2dbe
sangwookpark <sangwookp9591@gmail.com>
2025-09-09 00:40:28 +0900

    feat : 초기 로딩시 채널 유저 세팅

--- src/features/channel-initializer/ChannelInitializer.tsx
+++ src/features/channel-initializer/ChannelInitializer.tsx
@@ -1,16 +1,21 @@
 'use client';
 
+import { queries } from '@/entities/channel';
 import { useSocketStore } from '@/entities/message';
 import { useWorkspaceStore } from '@/shared/stores/workspace-store';
 import { useParams } from 'next/navigation';
 import { ReactNode, useEffect } from 'react';
+import { useMentionStore } from '../mention-user';
 
 export default function ChannelInitializer({ children }: { children: ReactNode }) {
   const params = useParams();
+  const wsSlug = params?.wsSlug as string;
   const chSlug = params?.chSlug as string;
 
   const { channels, currentChannel, setCurrentChannel, setCurrentDm } = useWorkspaceStore();
+  const { data: members, isLoading: isMemberLoading } = queries.useMembersBySlug(wsSlug, chSlug);
   const { joinRoom } = useSocketStore();
+  const { setMembers } = useMentionStore();
   const targetChannel = channels.find((c) => c.slug === chSlug);
 
   useEffect(() => {
@@ -21,6 +26,26 @@ export default function ChannelInitializer({ children }: { children: ReactNode }
       // 채널 소켓 연결
       joinRoom(targetChannel.id, 'channel');
     }
-  }, [targetChannel, currentChannel, setCurrentChannel, setCurrentDm, joinRoom]);
+
+    if (!isMemberLoading) {
+      console.log('setMembers!!! ', members);
+      const users = members?.map((member) => ({
+        id: member.user?.id,
+        name: member.user?.name || '',
+        email: member.user?.email,
+        profileImageUrl: member.user?.profileImageUrl,
+      }));
+      setMembers(users ?? []);
+    }
+  }, [
+    targetChannel,
+    currentChannel,
+    setCurrentChannel,
+    setCurrentDm,
+    joinRoom,
+    members,
+    isMemberLoading,
+    setMembers,
+  ]);
   return <>{children}</>;
 }
